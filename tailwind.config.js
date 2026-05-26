86847f189b1fdf4ad73e7cb05d1d54939ad94526
2028CZarut369 <2028CZarut369@stbhs.org>
2026-05-26 12:16:19 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./app/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        lavender: '#E8E0F0',
+        mint: '#D4EDDA',
+        peach: '#FFECD2',
+        sky: '#D6EAF8',
+        rose: '#FADBD8'
+      }
+    }
+  },
+  plugins: [],
+};
